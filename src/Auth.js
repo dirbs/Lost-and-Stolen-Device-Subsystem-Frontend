9e9db3d41e319f38b106965df3d4a99a8e1419a4
danishsarwar99 <49933911+danishsarwar99@users.noreply.github.com>
2020-10-06 13:27:54 +0500

    LSDS refresh problem resolved

--- src/Auth.js
+++ src/Auth.js
@@ -63,7 +63,7 @@ class Auth extends Component {
 			realm:realm,
 			clientId:clientId
 		});
-		keycloak.init({onLoad: 'login-required'}).success(authenticated => {
+		keycloak.init({onLoad: 'login-required', 'checkLoginIframe' : false}).success(authenticated => {
 			if(authenticated){
 				this.setState({keycloak: keycloak, authenticated: authenticated})
 				//Set token in local storage
